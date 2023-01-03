30b9f13f7a250b59d949702d239a60d5350784cc
madtro <anna.kozitzyna@yandex.ru>
2023-01-03 22:32:48 +0300

    task4

--- src/Main.java
+++ src/Main.java
@@ -11,17 +11,41 @@ public class Main {
         System.out.println("Task 2");
         char clientOS2 = 1;
         int clientDeviceYear = 2015;
-        int year = 2020;
-        if (clientOS2 == 1 && year <= clientDeviceYear) {
+        int yearDev = 2020;
+        if (clientOS2 == 1 && yearDev <= clientDeviceYear) {
             System.out.println("Установите версию приложения для Android по ссылке ниже");
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        } else if (clientOS2 == 1 && year > clientDeviceYear) {
+        } else if (clientOS2 == 1 && yearDev > clientDeviceYear) {
             System.out.println("Установите версию приложения для Android по ссылке ниже");
-        } else if (clientOS2 == 0 && year <= clientDeviceYear) {
+        } else if (clientOS2 == 0 && yearDev <= clientDeviceYear) {
             System.out.println("Установите версию приложения для iOS по ссылке ниже");
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
         } else {
             System.out.println("Установите версию приложения для iOS по ссылке ниже");
+        }
+
+        System.out.println("Task 3");
+        int year = 2200;
+        if (year % 100 == 0 && year % 400 == 0) {
+            System.out.println("Год " + year + " является високосным");
+        } else if (year % 4 == 0 && year % 100 != 0) {
+            System.out.println("Год " + year + " является високосным");
+        } else if (year % 100 == 0) {
+            System.out.println("Год " + year + " не является високосным");
+        } else {
+            System.out.println("Год " + year + " не является високосным");
+        }
+
+        System.out.println("Task 4");
+        int deliveryDistance = 95;
+        if (deliveryDistance < 20) {
+            System.out.println("Доставка займет сутки");
+        } else if (deliveryDistance < 60) {
+            System.out.println("Доставка займет двое суток");
+        } else if (deliveryDistance < 100) {
+            System.out.println("Доставка займет трое суток");
+        } else {
+            System.out.println("Доставка в вашу зону не осущетствляется");
+        }
     }
-}
 }
